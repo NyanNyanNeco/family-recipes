e2f97fafd208d62ed59ac7aa399d38c3d6d952fe
NyanNyanNeco <lemon791012@gmail.com>
2026-06-09 13:03:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "recipe-v1";
+const CACHE = "recipe-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
